6735fb90685b09fb6c591cef1a3b2473aa39f9e3
rohitkshirsagar19 <rohitkshirsagar1904@gmail.com>
2025-12-02 18:26:25 +0530

    feat: Added simple TUI
    Used Bubble Tea

--- main.go
+++ main.go
@@ -2,25 +2,93 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os/exec" // runing ext commands
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea" // Import Bubble Tea
 )
+
+// struct holds the "state" of application
+type model struct {
+	choice   []string // List of items in the menu
+	cursor   int      // Which item is currently highlighted
+	selected string   // Which item did the user choose
+}
+
+// initialModel define the starting state
+func initialModel() model {
+	return model{
+		choice:   []string{"Download video", "Download Audio Only", "Exit"},
+		cursor:   0, // Default top selected
+		selected: "",
+	}
+}
+
+// Init
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+// Update
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+
+		// Quit keys
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		case "down", "j":
+			if m.cursor < len(m.choice)-1 {
+				m.cursor++
+			}
+
+		case "enter":
+			m.selected = m.choice[m.cursor]
+			return m, tea.Quit
+		}
+	}
+
+	return m, nil
+}
+
+// View
+func (m model) View() string {
+	if m.selected != "" {
+		return fmt.Sprintf("\nYou selected: %s\nGood choice!\n", m.selected)
+	}
+
+	// Header
+	s := "\n What would you like to do?\n\n"
+
+	// Loop through choice and render them
+	for i, choice := range m.choice {
+		cursor := "" // Default empty
+		if m.cursor == i {
+			cursor = ">" // Highlighting the current row
+		}
+
+		// rendering the row
+		s += fmt.Sprintf("%s %s \n", cursor, choice)
+	}
+
+	// Footer (help text)
+	s += "\n (Use arrow keys to move, Enter to select)\n"
+	return s
+}
 
 func main() {
 
-	videoURL := "https://youtu.be/-iRTJj5g4Ks?si=QCFGsSoidy-j4jQf" // test video
+	// Start Bubble tea
+	p := tea.NewProgram(initialModel())
 
-	fmt.Println("Attempting to download video ...")
-
-	cmd := exec.Command("yt-dlp", "-f", "mp4", "-o", "test_video.mp4", videoURL)
-
-	output, err := cmd.CombinedOutput()
-
-	if err != nil {
-		log.Fatalf("Command failed: %v \n Output: %s", err, string(output))
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Error: %v", err)
+		os.Exit(1)
 	}
-
-	fmt.Println("Success!")
-	fmt.Println(string(output)) // printing yt-dlp logs
-
 }
